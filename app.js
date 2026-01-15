8538678beb893e95c66d83f1e5182ab9f557dd30
AmritReule123 <reuleamrit47@gmail.com>
2026-01-15 08:32:43 +0545

    add nodejs files

---
+++ app.js
@@ -0,0 +1,26 @@
+
+// const maa change garna meldaina 
+const a = 10
+const b = 20
+const sum = a + b
+console.log(sum)
+console.log("i am a user" )
+
+
+// Array
+const numbers =  [10 , 20, 40]
+// console.log(numbers[1])
+console.log(numbers.length)
+
+
+// Object 
+const person = {
+    name : "hari",
+    age : 30,
+    rollno: 12
+}
+console.log(person.age)     
+console.log(person.name)
+console.log("my name is haro" + person.name)
+
+
