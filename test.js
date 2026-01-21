9c72f423d88a9f01952c453a9801515dc7dcf1a6
Amrit Reule <reuleamrit47@gmail.com>
2026-01-21 02:25:30 +0000

    sdfds

---
+++ test.js
@@ -0,0 +1,12 @@
+console.log("i am a typescript langage")
+
+
+const studentDetails = {
+    name: "hari",
+    age: 20,
+    rollNo: 12,
+    number: 9807654321,
+    email: "hari@gmail.com"
+}
+
+console.log(studentDetails.name)
