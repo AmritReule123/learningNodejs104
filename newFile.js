e82fbd9476b21e9527570dbb1493ba693a3b126a
Amrit Reule <reuleamrit47@gmail.com>
2026-01-22 18:57:56 +0545

    change this format all

---
+++ newFile.js
@@ -0,0 +1,21 @@
+const a = 10
+const b = 20
+console.log("Sum =", a + b)
+
+// Array
+const numbers = [10, 20, 40]
+console.log("Array length =", numbers.length)
+
+// Object
+const person = {
+  name: "Hari",
+  age: 30,
+  rollno: 12
+}
+
+console.log(`My name is ${person.name}`)
+console.log(`Age is ${person.age}`)
+
+// Fruits
+const fruits = ["mango", "banana", "apple"]
+console.log("Second fruit:", fruits[1])
