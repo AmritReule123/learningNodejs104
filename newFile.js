51f67385b3e1ad859cabae15b1fdf31bb0970e6b
Amrit Reule <reuleamrit47@gmail.com>
2026-01-23 08:25:04 +0545

    most important code

--- newFile.js
+++ newFile.js
@@ -1,17 +1,34 @@
-const app = require ("express")()
+// const app = require ("express")()
 
 
-// home route 
+// // home route 
+// app.get("/", (req,res) =>{
+//     res.send("i am begginers freindley ")
+// })
+
+// // hero route 
+// app.get("/hero", (req,res) =>{
+//     res.send("make a complete this code runing")
+// })
+
+// // server
+// app.listen(4000, () =>{
+//     console.log("not a  a user only coder ")
+// })
+
+
+const app = require("express")()
+//home router
 app.get("/", (req,res) =>{
-    res.send("i am begginers freindley ")
+    res.send("this a home router")
 })
 
-// hero route 
-app.get("/hero", (req,res) =>{
-    res.send("make a complete this code runing")
+//hero server
+app.get("/rajan",(req,myname) =>{
+    myname.send("sdf fsdf dsf")
+
 })
 
-// server
-app.listen(4000, () =>{
-    console.log("not a  a user only coder ")
+app.listen(1212, () =>{
+    console.log("I am totally changed now1")
 })
